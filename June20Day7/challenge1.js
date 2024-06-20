0ba6f46915622cc83e477d5964c038756a413ec2
christinaduthie <christinarduthie@gmail.com>
2024-06-20 17:12:28 +0530

    June 20 Challenges

---
+++ June20Day7/challenge1.js
@@ -0,0 +1,47 @@
+// Find the common interest in books for the students
+
+function findCommonInterestsInBooks(students) {
+    const finalArray = {};
+    for (let i = 0; i < students.length; i++) {
+      const student = students[i];
+      for (let j = 0; j < student.books.length; j++) {
+       //console.log(student.books[j])
+       //console.log(finalArray)
+        if (!(student.books[j] in finalArray)) {
+          finalArray[student.books[j]] = [];
+        }
+        finalArray[student.books[j]].push(student.name);
+      }
+    }
+    return finalArray;
+  }
+
+const students = [
+  {
+    id: 0,
+    name: "Arun",
+    books: ["Wings of Fire", "Chakra"],
+  },
+  {
+    id: 1,
+    name: "Ashok",
+    books: ["Chakra", "War and Peace", "The Shining"],
+  },
+  {
+    id: 2,
+    name: "Balu",
+    books: ["Wings of Fire", "All about Cricket"],
+  },
+  {
+    id: 3,
+    name: "Cathi",
+    books: ["Against the wind", "The Shining", "War and Peace"],
+  },
+];
+//Function call
+const commonInterests = findCommonInterestsInBooks(students);
+//console.log(commonInterests);
+
+
+
+
